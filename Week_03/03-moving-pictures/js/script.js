14cf4f0fb795a2801cc4451444db9c2bd366cad3
mslineon <126345556+mslineon@users.noreply.github.com>
2023-09-21 16:56:02 -0400

    linking the two circles

--- Week_03/03-moving-pictures/js/script.js
+++ Week_03/03-moving-pictures/js/script.js
@@ -1,8 +1,8 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
@@ -15,27 +15,31 @@ let bg = {
     g: 0,
     b: 0
 };
+
 let circle1 = {
     x: 0,
     y: 250,
-    size: 100,
+    size: 50,
     fill: 255,
     alpha: 200
 };
+
 let circle2 = { // they are just ideas and not doing anything.
     x: 0,
     y: 250,
-    size: 75,
+    size: 50,
     fill: 255,
     alpha: 200
-
 };
 
-/**
- * Description of preload
-*/
-function preload() {
+let circle3 = {
+    x: 0,
+    y: 0,
+    size: 50,
+    fill: 50,
+    alpha: 100
 }
+
 
 
 /**
@@ -43,7 +47,6 @@ function preload() {
 */
 function setup() {
     createCanvas(500,500);
-    noStroke();
     circle2.x = width; // object properties are variables
     circle1.y = height/2;
 }
@@ -54,25 +57,37 @@ function setup() {
 */
 function draw() {
     //Background
+    //background(255);
     background(bg.r,bg.g,bg.b);
 
     bg.r = bg.r + 1;
     bg.g = bg.g + 0.5;
-    bg.b = bg.b +0.45;
+    bg.b = bg.b + 0.45;
 
-
-    bg.r = constrain(bg.r,0,213);
-    bg.g = constrain(bg.g,0,132);
-    bg.b = constrain(bg.b,0,219);
-
+    // this is the maximum at what value I want the bg color to change
+    bg.r = constrain(bg.r,0,220);
+    bg.g = constrain(bg.g,0,220);
+    bg.b = constrain(bg.b,0,220);
 
     //Circle1
+
+    //This is the movement of the circle
     circle1.x = circle1.x + 1;
-    circle1.x = constrain(circle1.x,0,width/2);
+    circle1.y = circle1.y - 0.45;
+
+    //Constrain
+    circle1.x = constrain(circle1.x,0,width);
+
+
+    //coloring
+
+    circle1.fill = random(0,255) - circle3.fill;
     fill(circle1.fill,circle1.alpha);
-    ellipse(circle1.x,circle1.y,circle1.size); // function
-    circle1.size = circle1.size + 1; 
+    ellipse(circle1.x,circle1.y,circle1.size/2); // function
+
+    circle1.size = circle1.size + 1;
     circle1.size = constrain(circle1.size,0,width);
+
 
     //Circle2
 
@@ -84,10 +99,23 @@ function draw() {
     //This is the color of the circle and the transparency
     fill(circle2.fill,circle2.alpha);
 
-    //This draws the circle
+    //This draws the circle #2
     ellipse(circle2.x,circle2.y,circle2.size);
 
     //This make the circle grow big
-    circle2.size = circle2.size + 0.45;
+    circle2.size = circle2.size + 0.10;
     circle2.size = constrain(circle2.size,0,width/2);
+
+
+    //This is circle 3
+    circle3.fill = random(0,255) - circle3.fill/3;
+    fill(circle3.fill);
+    ellipse(mouseX, mouseY, circle3.size);
+
+
+    // ellipse(mouseX+10, mouseY+10,circle3.size);
+    fill(255, 0, 0, 255);
+    stroke(0, 0, 5);
+    strokeWeight(2);
+    line(circle2.x - 1, width/2 , circle1.x +1, circle1.y - 0.45);
 }
--- exercices/i-like-to-move-it/js/script.js
+++ exercices/i-like-to-move-it/js/script.js
@@ -8,19 +8,48 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+// Global variables
 
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+};
+
+let circle1 = {
+    x: 0,
+    y: 250,
+    size: 50,
+    fill: 255,
+    alpha: 200
+};
+
+let circle2 = { // they are just ideas and not doing anything.
+    x: 0,
+    y: 250,
+    size: 50,
+    fill: 255,
+    alpha: 200
+};
+
+let circle3 = {
+    x: 0,
+    y: 0,
+    size: 50,
+    fill: 50,
+    alpha: 100
 }
+
 
 
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500,500);
+    noStroke();
+    circle2.x = width; // object properties are variables
+    circle1.y = height/2;
 }
 
 
@@ -28,5 +57,67 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    //Background
+    //background(255);
+    background(bg.r,bg.g,bg.b);
 
+    bg.r = bg.r + 0.5;
+    bg.g = bg.g + 0.5;
+    bg.b = bg.b + 0.5;
+
+    // this is the maximum at what value I want the bg color to change
+    bg.r = constrain(bg.r,0,220); 
+    bg.g = constrain(bg.g,0,220);
+    bg.b = constrain(bg.b,0,220);
+
+    //Circle1
+
+    //This is the movement of the circle
+    circle1.x = circle1.x + 0.5;
+    circle1.y = circle1.y - 0.5;
+    
+    //Constrain
+    circle1.x = constrain(circle1.x,0,width);
+    circle1.y = constrain(circle1.y,height,0);
+    
+
+    //coloring
+
+    circle1.fill = random(0,255) - circle3.fill;
+    fill(circle1.fill,circle1.alpha);
+    ellipse(circle1.x,circle1.y,circle1.size/2);    // function
+
+    circle1.size = circle1.size + 0.5;
+    circle1.size = constrain(circle1.size,0,width);
+
+
+    //Circle2
+
+    //this make the circle move from right to left
+
+    circle2.x = circle2.x - 0.5; // This is the movement of my circle
+    circle2.x = constrain(circle2.x,width/2,width);//minimum constrain start at the middle of the width
+
+    //This is the color of the circle and the transparency
+    fill(circle2.fill,circle2.alpha);
+
+    //This draws the circle #2
+    ellipse(circle2.x,circle2.y,circle2.size);
+
+    //This make the circle grow big
+    circle2.size = circle2.size + 0.10;
+    circle2.size = constrain(circle2.size,0,width/2);
+
+
+    //This is circle 3
+    circle3.fill = random(0,255);
+    fill(circle3.fill);
+    ellipse(mouseX, mouseY, circle3.size);
+
+    
+    // ellipse(mouseX+10, mouseY+10,circle3.size);
+    fill(255, 0, 0, 255);
+    stroke(0, 0, 5);
+    strokeWeight(3);
+    line(circle2.x - 1, width/2 , circle1.x, circle1.y);
 }
