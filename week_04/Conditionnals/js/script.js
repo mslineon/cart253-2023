d13661d5fd2ee7ef8fc6a70b0bf38209bb9c5964
mslineon <126345556+mslineon@users.noreply.github.com>
2023-09-30 20:05:38 -0400

    conditional + more conditional

--- week_04/Conditionnals/js/script.js
+++ week_04/Conditionnals/js/script.js
@@ -1,32 +1,100 @@
 /**
- * Title of Project
- * Author Name
+ * Week 04 - Conditional
+ * Line On
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0,
+    speed: 0.5
+}
 
+let circle = {
+    x: 0,
+    y: 255,
+    size: 50,
+    speed: 1,
+    fill: 255
 }
 
 
-/**
- * Description of setup
-*/
+
+
 function setup() {
+    // This is the canva size
+    createCanvas(500, 500);
 
 }
 
 
-/**
- * Description of draw()
-*/
+
 function draw() {
+
+    // This is the color of my background
+    background(bg.r,bg.g,bg.b);
+
+    bg.r = bg.r + bg.speed;
+    bg.g = bg.g + bg.speed;
+    bg.b = bg.b + bg.speed;
+
+    bg.r = constrain(bg.r,0,255);
+    bg.g = constrain(bg.g,0,204);
+    bg.b = constrain(bg.b,0,13);
+
+      // NEW: If the background shade is exactly 255 (white, the maximum color number)
+    if (bg.r === 255) {
+        // Set it back to 0 (black)
+        bg.r = 0;
+        bg.g = 0;
+        bg.b = 0;
+    }
+
+    if (circle.x > width){
+        circle.speed = -circle.speed;
+    }
+
+    if (circle.x < 0){
+        circle.speed = -circle.speed;
+    }
+    
+
+
+    fill(circle.fill);
+
+    if(mouseX < width/4) {
+        circle.fill = 150;
+      }
+
+    else if (mouseX < width/2){
+        circle.fill = 0;
+    }
+
+    // strict conditions 
+    else if (mouseX < 3 * width/4) {
+        circle.fill = 150;
+        if (circle.x > width/2){
+            circle.size = 100;
+        }
+      }
+
+    else {
+        circle.fill = 255;
+        if (circle.x < width/2){
+            circle.size = 50;
+        }
+      }
+
+    // if (mouseX < width/2){
+    //     circle.fill = 255;
+    // }
+
+
+    circle.x = circle.x + circle.speed;
+    ellipse(circle.x,circle.y,circle.size);
+
 
 }
