cf81eb56a31bac42f418d5b5e2eccfefc49881a7
mslineon <126345556+mslineon@users.noreply.github.com>
2023-10-01 15:31:47 -0400

    Practicing Boleans conditions

--- week_04/boleans/js/script.js
+++ week_04/boleans/js/script.js
@@ -1,32 +1,55 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Boleans Tutorial
+ * Line On
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let displayCircle = false;
+
+let circle1 = {
+    x: 0,
+    y: 0,
+    size: 100,
+    fill: {
+        r:125,
+        g:50,
+        b:76
+    }
 
 }
 
-
-/**
- * Description of setup
-*/
+//This is my canvas
 function setup() {
 
+    createCanvas (windowWidth,windowHeight);
+    circle1.x = width/3;
+    circle1.y = height/3;
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+
+        if (mouseIsPressed) {
+          background(255);
+          displayCircle = true;
+        }
+        else {
+          background(0);
+        }
+
+         if (displayCircle) {
+          ellipse(width/2,height/2,100,100);
+        }
+
+        if (keyIsPressed) {
+          background (255);
+          fill(circle1.fill.r,circle1.fill.g,circle1.fill.b);
+          ellipse (circle1.x,circle1.y,circle1.size);
+        }
+
+        else {
+            fill(175);
+            ellipse (circle1.x,circle1.y,circle1.size);
+        }
 
 }
