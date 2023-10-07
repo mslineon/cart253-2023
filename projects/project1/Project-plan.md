ee3121932ece0046b8122a4b60e273c52d41e540
mslineon <126345556+mslineon@users.noreply.github.com>
2023-10-07 17:12:35 -0400

    create planning of the game & set up base

---
+++ projects/project1/Project-plan.md
@@ -0,0 +1,60 @@
+# Template p5 project
+
+Phase 1:
+
+Assets needed
+
+General idea
+
+	1. My dog catching 
+	2. Bones
+	3. Users throw the ball
+	4. Wearing glass when wins when dogs catch bones
+     
+	
+Interactivity
+
+	• Ball throwing:
+        -  Click, drag & release - get distance in-between mouse press & release using the dist function 
+
+    • Bones falls from the hands when space buttons is clicked
+        - hand is image 
+        - hand moves on x axis with noise
+
+    • Timer to catch 3 of each balls/bones
+        - number that counts down using variable
+
+    • Display an amount of bones/ball caught
+        - create variables of the amount of bones (amountOfBones)
+        - create variables of the amount of balls (amountOfBalls)
+        - everytime Abi & ball or bones touches, then the number of bones/balls is counted
+
+    • Abi
+        - walk straight back & forth on the x axis
+        - variable to see the intersect abi & ball
+        - variable to see the intersect abi & bones
+
+    • States:
+
+        - Title screen
+        - Explain controls 
+        - Simulation
+        - Failure
+            - 1, 2, 3 bone
+            - 1, 2, 3 ball
+            - enough bones and balls
+
+
+2 movings parts 
+	• controlled by the users
+	• Maybe object are floating around?
+
+	
+Simulation:
+
+	• Moving bones
+	• Users Throwing balls
+	• Dog touches bones = Happy Abi
+	• Dog touches balls = Happy Abi
+	• Dog missed = sad 
+
--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,32 +1,89 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Project 1 - Simulation
+ * Line On
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
+let state = `explanation`; // variable to store the state open screen 
+
+
+
+
 function preload() {
 
 }
 
 
-/**
- * Description of setup
-*/
+
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    textAlign(CENTER,CENTER); // put text middle
+}
+
+function draw() {
+
+    if (state === `title`) {
+        title();
+    }
+    else if (state === `explanation`) {
+        explanation();
+    }
+    else if(state === `simulation`) {
+        simulation();
+    }
+    else if(state === `failure`) {
+        failure();
+    }
+
 
 }
 
+function title(){
+    push(); 
+    textSize(100); // make title big
+    fill(120,153,212); // make title bluey
+    text(`WELCOME TO ABIGAME`,width/2,height/2); // display text
+    textSize(50);
+    text(`press any key to learn the controls`,width/2,height/2+70);
+    pop();
+}
 
-/**
- * Description of draw()
-*/
-function draw() {
+function explanation(){
+    push();
+    textSize(45);
+    fill(120, 153, 212);
+    text(`space to drop the bones`, width/2, height/2 - 45);
+    text(`click mouse to shoot the ball`, width/2, height/2);
+    text(`press any key to start`, width/2, height/2 + 45);
+    pop();
+
+}
+
+function simulation() {
+
+}
+
+function failure() {
+
+}
+
+function ballControl() {
+
+}
+
+function boneControl() {
+
+}
+
+function timerDisplay() {
+
+}
+
+function scoreDisplay() {
+
+}
+
+function abiDog() {
 
 }
