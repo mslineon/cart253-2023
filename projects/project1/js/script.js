591f635fe1932866d874d2c9cd3ac9c9971bf5a3
mslineon <126345556+mslineon@users.noreply.github.com>
2023-10-09 19:03:44 -0400

    add images, objects & for abi hand and bone

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -5,6 +5,8 @@
 
 "use strict";
 
+let bgImg;
+
 let state = `simulation`; // variable to store the state open screen 
 
 let gameTimer = {
@@ -16,27 +18,31 @@ let gameTimer = {
 let abiDog = {
     x: 0,
     y: undefined,
-    size: 50,
-    vx: 1,
+    size: 300,
+    vx: 6,
     vy: 0,
-    abiImg: undefined
+    abiImg: undefined,
+    scale: 1, // flipping img reference from p5js from a snail example: https://editor.p5js.org/creativecoding/sketches/0JBTBmvGb
+    abiBoneImg: undefined,
+    hasBone: false
 };
+
 
 let hand = {
     x: 0,
-    y: undefined,
-    size: 35,
-    vx: 1,
+    y: 75,
+    size: 140,
+    vx: 10,
     vy: 0,
     handImg: undefined
 };
 
 let bone = {
     x: 0,
-    y: 0,
-    size: 30,
-    vx: 1,
-    vy: 0,
+    y: -100,
+    size: 100,
+    vx: 0,
+    vy: 2,
     boneImg: undefined
 };
 
@@ -49,26 +55,30 @@ let ball = {
     ballImg: undefined
 };
 
-
-
-
 function preload() {
-    abiImg = loadImage("assets/images/Abigail.png");
-    boneImg = loadImage("assets/images/Bone.png");
-    handImg = loadImage("assets/images/Hand.png");
-    ballImg = loadImage("assets/images/Ball.png");
+    abiDog.abiImg = loadImage("assets/images/Abigail.png");
+    bone.boneImg = loadImage("assets/images/Bone.png");
+    hand.handImg = loadImage("assets/images/Hand.png");
+    ball.ballImg = loadImage("assets/images/Ball.png");
+    abiDog.abiBoneImg = loadImage("assets/images/Abigail-bone.png");
+    bgImg = loadImage("assets/images/Bg.jpeg");
 }
 
 
 
 function setup() {
+
     createCanvas(windowWidth, windowHeight);
     textAlign(CENTER,CENTER); // put text middle
+    abiDog.y = height/2;
+    imageMode(CENTER);
+    
+    
 }
 
 function draw() {
 
-    background(255);
+    image(bgImg,0,0);
 
     if (state === `title`) {
         title();
@@ -82,10 +92,6 @@ function draw() {
     else if(state === `failure`) {
         failure();
     }
-
-    
-
-
 }
 
 function title(){
@@ -106,35 +112,62 @@ function explanation(){
     text(`click mouse to shoot the ball`, width/2, height/2);
     text(`press any key to start`, width/2, height/2 + 45);
     pop();
-
 }
 
-function simulation() {
-    timerDisplay();
+function simulation() { 
+    //timerDisplay();
+    // abiDogDisplay();
+    // boneHandDisplay();
+    // boneTouchAbi();
 }
 
 function failure() {
 
 }
 
+//Part of the simulation
 function ballControl() {
 
 
 }
 
-function boneControl() {
+function boneHandDisplay() {
+    hand.x += hand.vx;
+    bone.y += bone.vy;
 
+    if (hand.x > width || hand.x < 0) {
+        hand.vx = -hand.vx;
+    }
+    
+    push();
+    image(hand.handImg, hand.x, hand.y, hand.size, hand.size);
+    pop();
+    image(bone.boneImg,bone.x, bone.y,bone.size,bone.size);
+}
+
+function boneTouchAbi() {
+    let d = dist(bone.x, bone.y, abiDog.x, abiDog.y);
+
+    if (d < bone.size/4 + abiDog.size/4) {
+        abiDog.hasBone = true;
+
+    }
+}
+
+function keyPressed() {
+        bone.x = hand.x; 
+        bone.y = hand.y;
+        abiDog.hasBone = false;
 }
 
 function timerDisplay() {
     gameTimer.secondsCounter -= 1/60; // 60 frame per 1 second 
-    gameTime.minuteCounter = gameTimer.secondsCounter/60; // This is the minutes counting
+    gameTime.minuteCounter = gameTimer.secondsCounter/60; // Minutes counting
 
     if (gameTimer.secondsCounter < 0) { // if the counter gets to zero then initiate the failure sequence
         state = `failure`;
-        timeLeftState = false; // You ran out of time, haha to bad for you
+        timeLeftState = false; // You ran out of time, haha too bad for you
     }
-    
     // displaying time bottom left of the screen seconds & minute so it looks like a clock
     push();
     textAlign(LEFT); 
@@ -149,5 +182,24 @@ function scoreDisplay() {
 }
 
 function abiDogDisplay() {
+    abiDog.x += abiDog.vx;
+
+    if (abiDog.x > width || abiDog.x < 0) {
+        abiDog.vx = -abiDog.vx;
+        abiDog.scale = -abiDog.scale;
+    }
+    
+    push();
+    scale (abiDog.scale, 1); // flipping img reference from p5js from a snail example: https://editor.p5js.org/creativecoding/sketches/0JBTBmvGb
+
+    //
+    if (abiDog.hasBone === false) {
+        image(abiDog.abiImg, abiDog.x * abiDog.scale, abiDog.y, abiDog.size, abiDog.size);
+    }
+    else if (abiDog.hasBone === true) {
+        image(abiDog.abiBoneImg, abiDog.x * abiDog.scale, abiDog.y, abiDog.size, abiDog.size);
+        bone.x = width * 2;
+    }
+    pop();
 
 }
